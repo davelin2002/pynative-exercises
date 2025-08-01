d7ec2eb94677494a292f60b4442dc2bf051b9208
David Lin <davidlin200209@gmail.com>
2025-08-01 17:32:37 -0400

    adding new files

--- 01-beginner-exercises/03_print_even_index.py
+++ 01-beginner-exercises/03_print_even_index.py
@@ -11,10 +11,19 @@ def print_even_index_1():
 
 # attempting the problem with string slicing
 def print_even_index_2():
-    pass
+    word = input("enter a word:")
+
+    # easier to iterate over a list
+
+    word_list = list(word)
+
+    for letter in word_list[::2]:
+        print(letter)
 
 def main():
+    print("output from standard and list slicing implementations:")
     print_even_index_1()
+    print_even_index_2()
 
 if __name__ == "__main__":
     main()
