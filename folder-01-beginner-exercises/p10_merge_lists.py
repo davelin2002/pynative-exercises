edd006921c6d5ce2e3ef972e300a7d775dade9fb
David Lin <davidlin200209@gmail.com>
2025-09-02 13:51:38 -0400

    added updated p10 file

--- folder-01-beginner-exercises/p10_merge_lists.py
+++ folder-01-beginner-exercises/p10_merge_lists.py
@@ -6,3 +6,32 @@
 # list1 = [10, 20, 25, 30, 35]
 # list2 = [40, 45, 60, 75, 90]
 
+def merge_even_odd(l1, l2):
+    l1_length = len(l1)
+    l2_length = len(l2)
+    final_list = []
+
+    longest_list = max(l1_length, l2_length)
+
+    for num in range(longest_list):
+        if l1[num] % 2 == 1:
+            final_list.append(l1[num])
+        if l2[num] % 2 == 0:
+            final_list.append(l2[num])
+    
+    return final_list
+        
+           
+
+def main():
+    list1 = [10, 20, 25, 30, 35]
+
+    list2 = [40, 45, 60, 75, 90]
+
+    # expected output: [40, 25, 60, 35, 90]
+
+    print(merge_even_odd(list1, list2))
+
+if __name__ == "__main__":
+    main()
+
